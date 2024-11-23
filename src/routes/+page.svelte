53903dee7a5f263ea89ace5a4023ab3215c04e78
Curtis Parfitt-Ford <curtis@mashedkeyboard.me>
2024-11-23 20:43:43 +0000

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import something from '../test.scss?inline';
+</script>
+
+<svelte:head>
+    <svelte:element this={'style'} type="text/css">{something}</svelte:element>
+</svelte:head>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
